c1a460bba4eb3988bf4a55af6579a49af80eed4f
Ivan A. Sergeev <ivan@permianlabs.xyz>
2025-12-02 04:28:40 -0600

    script: add DeployBundleCollateralWrapper script

---
+++ script/DeployBundleCollateralWrapper.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.29;
+
+import "forge-std/Script.sol";
+
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {BundleCollateralWrapper} from "src/collateralWrappers/BundleCollateralWrapper.sol";
+
+import {Deployer} from "./utils/Deployer.s.sol";
+
+contract DeployBundleCollateralWrapper is Deployer {
+    function run(
+        address deployer
+    ) public broadcast useDeployment returns (address) {
+        // Deploy BundleCollateralWrapper implementation
+        BundleCollateralWrapper bundleCollateralWrapperImpl = new BundleCollateralWrapper();
+        console.log("BundleCollateralWrapper implementation", address(bundleCollateralWrapperImpl));
+
+        // Deploy BundleCollateralWrapper proxy
+        TransparentUpgradeableProxy bundleCollateralWrapper =
+            new TransparentUpgradeableProxy(address(bundleCollateralWrapperImpl), deployer, "");
+        console.log("BundleCollateralWrapper proxy", address(bundleCollateralWrapper));
+
+        _deployment.bundleCollateralWrapper = address(bundleCollateralWrapper);
+
+        return (address(bundleCollateralWrapper));
+    }
+}
